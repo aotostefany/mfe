4784fc4cccfad764219e0088fbecd9af9f64c3f0
aotostefany <stafany.arruiz@gmail.com>
2023-10-28 10:42:02 -0300

    test

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
+import { Switch, Router, BrowserRouter, Route } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
