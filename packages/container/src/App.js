f3746028b7794ce090241cdca44a47b90365ffd8
aotostefany <stefany.arruiz@gmail.com>
2023-11-09 14:38:51 -0300

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/header';
 
 export default () => {
+<<<<<<< Updated upstream
   return (
     <div>
       <h1>Testing</h1>
       <MarketingApp />
     </div>
   );
+=======
+<<<<<<< Updated upstream
+  return <MarketingApp />;
+=======
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  );
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 };
